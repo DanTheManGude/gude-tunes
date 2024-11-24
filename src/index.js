28135d5b8742fefee791f1329dc54db4e0e5f226
DanTheManGude <dgude31@gmail.com>
2024-11-24 14:54:15 -0700

    fix render usage

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <h1 id="title">Gude Tunes</h1>
-    <h3 id="subtitle">Utilities to enhance your Spotify‎ experience</h3>
+    <h3 id="subtitle">Utilities to enhance your Spotify experience</h3>
     <h5 id="help-text">
       Be sure to have Spotify recently paused or playing for most utilities.
     </h5>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 console.log(`Git hash: ${process.env.REACT_APP_VERSION}`);
